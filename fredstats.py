b899435c5f1b5bc0547d913695ebf1d800cc5fc6
Fredrick Lambuth <flambuth@gmail.com>
2019-10-08 08:55:05 -0500

    now im really done. really

--- fredstats.py
+++ fredstats.py
@@ -23,4 +23,55 @@ def make_df_from_db(url, table):
 # emps_df = pd.read_sql('SELECT * FROM employees', url)
 # salary_df = pd.read_sql('SELECT * FROM salaries', url)
 
+# Use the employees database.
+url = get_db_url(user,host,password,'employees')
 
+def kevins_query():
+    """ Makes 3 Columns that are the difference of days between two date values:
+    """
+    employees = pd.read_sql('''SELECT
+        e.*,
+        datediff('2002-09-30', e.hire_date) tenure,
+        t.title,
+        t.from_date title_from,
+        datediff('2002-09-30', t.from_date) t_tenure,
+        et.titles,
+        s.salary,
+        s.from_date salary_from,
+        datediff('2002-09-30', s.from_date) s_tenure,
+        es.salaries
+    FROM
+        employees e
+    JOIN 
+        titles t 
+        USING(emp_no)
+    JOIN 
+        salaries s 
+        USING(emp_no)
+    JOIN
+        (SELECT 
+            emp_no,
+            count(*) titles
+        FROM
+            titles
+        GROUP BY
+            emp_no) et
+        USING(emp_no)
+    JOIN
+        (SELECT 
+            emp_no,
+            count(*) salaries
+        FROM
+            salaries
+        GROUP BY
+            emp_no) es
+        USING(emp_no)
+    WHERE
+        s.to_date > '2002-09-30'
+        AND t.to_date > '2002-09-30';
+    ''', url)
+
+# Is there a relationship between how long an employee has been with the company and their salary?
+
+# Is there a relationship between how long an employee has been with the company and the number 
+# of titles they have had?
